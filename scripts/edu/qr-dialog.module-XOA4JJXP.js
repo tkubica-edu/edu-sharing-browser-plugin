5af0ee3ec3cf2e44f3ce36d4d97bf6f817bd1edd
tkubica-edu <kubica@edu-sharing.net>
2026-08-04 17:03:02 +0200

    chore: update packaged edu-sharing web-component bundle

---
+++ scripts/edu/qr-dialog.module-XOA4JJXP.js
@@ -0,0 +1,1 @@
+import{b as a,c as b}from"./chunk-LPJPWV6O.js";import"./chunk-PA74ZTMV.js";import"./chunk-UMPK5M6A.js";import"./chunk-KB7PPF6E.js";import"./chunk-K5TY7LKG.js";import"./chunk-HQGMLCMR.js";import"./chunk-47JZTFP6.js";import"./chunk-7SRXXGKM.js";import"./chunk-OQNJ47OW.js";import"./chunk-SISH7RCI.js";export{a as QrDialogComponent,b as QrDialogModule};
